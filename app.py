f34fe7b81054d28100ebc20769fa8a7574a4acc5
Venkat Jawahar Reddy Yerrabathini <JawaharReddy@csu.fullerton.edu>
2023-05-08 02:46:09 -0700

    Added Some basic HTML Code

--- app.py
+++ app.py
@@ -1,28 +1,30 @@
 from flask import Flask, request, jsonify
 import pymysql
 import os
+from sqlalchemy import create_engine
+
+app = Flask(__name__)
+
+# MySQL Configuration
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root@127.0.0.1:51559/flask-crud-app'
+
+# Create SQLAlchemy engine
+engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+
+# Create pymysql connection
+mysql = pymysql.connect(
+    host=engine.url.host,
+    port=engine.url.port,
+    user=engine.url.username,
+    password=engine.url.password,
+    db=engine.url.database
+)
 
 #************IMP INFO*****************
 # HOST="aws.connect.psdb.cloud"
 # USERNAME="mtcvbf1one39c8kpl112"
 # PASSWORD="pscale_pw_M6L8nuznl6Xr3zbtR7GBUJm5vhMcGZ5YbZK4wVoRGdS"
 # DATABASE="flask-crud-app"
-
-app = Flask(__name__)
-
-# MySQL Configuration
-app.config['MYSQL_HOST'] = os.getenv('HOST')
-app.config['MYSQL_USER'] = os.getenv('USERNAME')
-app.config['MYSQL_PASSWORD'] = os.getenv('PASSWORD')
-app.config['MYSQL_DB'] = os.getenv('DATABASE')
-
-mysql = pymysql.connect(
-    host=app.config['MYSQL_HOST'],
-    user=app.config['MYSQL_USER'],
-    password=app.config['MYSQL_PASSWORD'],
-    db=app.config['MYSQL_DB']
-)
-
 
 # Opened cursor to execute MySQL quiries 
 cursor = mysql.cursor()
